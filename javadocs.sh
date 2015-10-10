f7fcfc509252c80a4eaacb633645086d1772f651
Hai Bison <haibison.apps@gmail.com>
2015-10-11 06:17:05 +0700

    Updated API docs for v6.0.0

---
+++ javadocs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/sh
+
+#
+#   Copyright 2012 Hai Bison
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+javadoc -d apidocs/ -sourcepath ../source/app/src/main/java/:../source/app/build/generated/source/buildConfig/release/:../source/app/build/generated/source/r/release/ -subpackages haibison -linkoffline https://developer.android.com/reference ../android-sdk-linux/docs/reference -classpath ../android-sdk-linux/platforms/android-23/android.jar:../android-sdk-linux/extras/android/support/annotations/android-support-annotations.jar
